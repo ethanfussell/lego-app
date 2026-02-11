c34496c326b03833197b872ae69bf4be3b26f36a
ethanfussell <ethanfussell@icloud.com>
2026-02-10 21:11:56 -0500

    SEO: fix account title double site name

--- frontend_next/app/account/page.tsx
+++ frontend_next/app/account/page.tsx
@@ -3,7 +3,7 @@ import AccountClient from "./AccountClient";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Account | LEGO App",
+  title: "Account",
 };
 
 export default function Page() {
