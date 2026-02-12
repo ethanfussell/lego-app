cfd1f0a256ffc07139cf65deb1e113107d7ceda6
ethanfussell <ethanfussell@icloud.com>
2026-02-11 20:18:29 -0500

    SEO: collection page title uses layout template

--- frontend_next/app/(authed)/collection/page.tsx
+++ frontend_next/app/(authed)/collection/page.tsx
@@ -1,9 +1,9 @@
 // app/(authed)/collection/page.tsx
-import CollectionClient from "./CollectionClient";
 import type { Metadata } from "next";
+import CollectionClient from "./CollectionClient";
 
 export const metadata: Metadata = {
-  title: "My Collection | LEGO App",
+  title: "My Collection",
 };
 
 export default function CollectionPage() {
